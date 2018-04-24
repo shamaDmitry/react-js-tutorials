fc26e89b1fded2b56349df6d8920e6a961b06136
kirill <kirill.karetovitch@gmail.com>
2018-04-24 17:09:06 +0300

    first1

--- 2-react-router/src/js/pages/Featured.js
+++ 2-react-router/src/js/pages/Featured.js
@@ -17,7 +17,9 @@ export default class Featured extends React.Component {
       "Some Other Article",
       "Yet Another Article",
       "Still More",
-    ].map((title, i) => <Article key={i} title={title}/> );
+    ].map(function (title, i) {
+        return <Article key={i} title={title}/>;
+    });
 
     const adText = [
       "Ad spot #1",
